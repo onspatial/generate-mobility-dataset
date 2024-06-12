9d6f9ef9f80eab788fd9a47f3b33827f9f01762f
amirih <hossein.amiri.enterprise@gmail.com>
2024-06-12 16:54:13 -0400

    update

--- src/main/python/code/maps.py
+++ src/main/python/code/maps.py
@@ -1,9 +1,10 @@
 import utils.pqgis as pqgis
 
 if __name__ == '__main__':
-    map_name = 'test'
-    output_folder = 'data/output/' + map_name 
+    # map_name = 'test'
     # bounding_box = geo.get_bounding_box(map_name)
+    # output_folder = 'data/output/' + map_name 
+    output_folder = 'data/maps/test'
     bounding_box = [-84.41213984, 33.72878582, -84.36418537, 33.76304255]
     pqgis.generate_map(bounding_box, output_folder, new_map=True)
     print('Map generated successfully')
