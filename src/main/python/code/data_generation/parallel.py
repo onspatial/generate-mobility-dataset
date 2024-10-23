3bd5799b1a42ad066edb17fb7037ad6087bff9f5
Hossein Amiri <hossein.amiri.enterprise@gmail.com>
2024-10-23 10:05:58 -0400

    Update parallel.py

--- src/main/python/code/data_generation/parallel.py
+++ src/main/python/code/data_generation/parallel.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
    
    
     input_parameters = params.get_from_json('params.input.json')
-    simulated_params = run(input_parameters, shuffle=False, fork_join=True,  parallel=8)
-    params.save_params_to_file(simulated_params, f"params.simulated.json")
+    simulated_params = run(input_parameters, shuffle=False, 
+                           fork_join=True,  parallel=8)
+    params.save_params_to_file(simulated_params, 
+                               f"params.simulated.json")
     print("Results Generated successfully!")
 
